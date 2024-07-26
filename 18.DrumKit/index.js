0736192cf37f86679a80008ca7cec663ec99d5e2
Florian Wiesner <florian.wiesner@prounix.de>
2024-07-26 18:18:20 +0200

    Added project files

--- 18.DrumKit/index.js
+++ 18.DrumKit/index.js
@@ -1,4 +1,9 @@
-document.querySelector("button").addEventListener("click", handleEvent);
+for(var i=0; i<document.querySelectorAll(".drum").length; i++) {
+
+    document.getElementsByTagName("button")[i].addEventListener("click", handleEvent);
+
+}
+
 
 function handleEvent(){
     alert("I've got clicked");
